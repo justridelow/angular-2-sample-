82a016f138a9b3075d87d26cc80324c909fbba18
Delgado <212550466@logon.ds.ge.com>
2016-03-04 14:19:57 -0600

    Chapter 3

---
+++ app/hero.js
@@ -0,0 +1,13 @@
+/**
+ * Created by 212550466 on 3/4/2016.
+ */
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters:[],
+        execute: function() {
+        }
+    }
+});
+//# sourceMappingURL=hero.js.map
---
+++ app/hero.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by 212550466 on 3/4/2016.
+ */
+
+export interface Hero
+{
+    id: number;
+    name: string;
+}
